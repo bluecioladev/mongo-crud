a64bddc69ac7dab13124d52e4701888c17c71315
jarb <jarb001@gmail.com>
2021-03-24 23:32:00 -0400

    creada la conexion

---
+++ src/model/task.js
@@ -0,0 +1,31 @@
+// module.exports = function () {
+  
+//        var db = require('../libs/db-conecction')();
+
+//         var Schema = require('mongoose').Schema;
+//         //var mongoose = require('mongoose'); 
+//         var Task = mongoose.Schema({
+//             title: String,
+//             description: String,
+//             status: Boolean
+
+
+//         });
+//         return db.mongoose.model('task', Task);
+
+
+   
+// }
+module.exports = function(){
+
+    var db = require('../libs/db-conecction')();
+    var Schema = require('mongoose').Schema;
+
+    var Task = Schema({
+        title: String,
+        description: String,
+        status: Boolean
+    });
+    return db.model('tasks',Task);
+}
+
---
+++ src/routes/index.js
@@ -0,0 +1,41 @@
+// const express = require('express');
+
+// const router = express.Router();
+
+// //const model =require('../model/task')();
+// (async () => {
+
+//    const model =  require('../model/task')();
+
+
+//     router.get('/', (req, res) => {
+//         model.find({}, (err, tasks) => {
+//             if (err) throw err;
+//             res.render('index', {
+
+//                 title: 'CRUD',
+//                 task: tasks
+//             });
+//         });
+//     });
+
+// })
+
+// module.exports = router;
+
+const express = require('express');
+const router = express.Router();
+
+const model = require('../model/task')();
+
+router.get('/' ,(req, res) => {
+    model.find({}, (err, tasks) =>{
+        if (err) throw err;
+        res.render('index', {
+            title: 'CRUD',
+            task: tasks
+        });
+    });
+});
+
+module.exports = router;
